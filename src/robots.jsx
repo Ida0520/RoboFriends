f86d1fedbefb3cdc966d8394bbaa656410c2aaa9
Ida0520 <157887603+Ida0520@users.noreply.github.com>
2025-05-31 15:36:11 +0200

    My RoboFriends Project

---
+++ src/robots.jsx
@@ -0,0 +1,70 @@
+export const robots = [
+	{
+		id:1,
+		name: 'Lava',
+		username: 'PastaLava222',
+		email: 'pastalava@gamil.com'
+	},
+	{
+		id:2,
+		name: 'Moomin',
+		username: 'MoominMoomin',
+		email: 'moomin55@gmail.com'
+	},
+	{
+
+		id:3,
+		name: 'Shaquille',
+		username: 'Shaquille_outmeal',
+		email: 'shaquille08@gmail.com'
+	},
+	{
+
+		id:4,
+		name: 'Noelle',
+		username: 'Santas_number1_elf',
+		email: 'noelle@gmail.com'
+	},
+	{
+
+		id:5,
+		name: 'Peter',
+		username: 'iNeed2p',
+		email: 'peterned@gmail.com'
+	},
+	{
+
+		id:6,
+		name: 'Flynn',
+		username: 'findtheLynn',
+		email: 'laflynn@gmail.com'
+	},
+	{
+
+		id:7,
+		name: 'brad',
+		username: 'Breadpitt',
+		email: 'brad11@gmail.com'
+	},
+	{
+
+		id:8,
+		name: 'Frosty',
+		username: 'CerealKiller',
+		email: 'Frosty2@gmial.com'
+	},
+	{
+
+		id:9,
+		name: 'Xabi',
+		username: 'NotToXabi',
+		email: 'waxabi@gmail.com'
+	},
+	{
+
+		id:10,
+		name: 'Mary',
+		username: 'BloodyMary',
+		email: 'maryjane07@gmail.com'
+	}
+]
